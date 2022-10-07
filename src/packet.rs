3089d7c7550f4a1217c74ca50476b1dd86f737b5
Yeri Kim <laplacianmatrix@gmail.com>
2022-10-07 07:00:35 +0000

    `src/packet.rs`: asdf

--- src/packet.rs
+++ src/packet.rs
@@ -101,6 +101,7 @@ impl Packet {
             .await?;
         stream.lock().await.write_all(&body).await?;
         stream.lock().await.write_all(&checksum).await?;
+        stream.lock().await.write_all(&['\0' as u8]).await?;
         stream.lock().await.flush().await?;
         Ok(())
     }
@@ -119,12 +120,13 @@ impl Packet {
         );
         to_send.append(&mut body);
         to_send.append(&mut checksum.to_vec());
+        to_send.push('\0' as u8);
         sender.try_send(to_send).ok();
     }
 
     pub async fn from_stream(stream: Arc<Mutex<TcpStream>>) -> async_std::io::Result<Self> {
         let mut buf: [u8; HEADER_SIZE] = [0; HEADER_SIZE];
-        stream.lock().await.read(&mut buf).await?;
+        stream.lock().await.read_exact(&mut buf).await?;
         debug!("{:?}", buf);
         if let Ok(header) = bincode::DefaultOptions::new()
             .with_big_endian()
@@ -134,10 +136,10 @@ impl Packet {
             if header.check_magic() {
                 let mut body: Vec<u8> = Vec::new();
                 body.resize(header.length as usize, 0);
-                stream.lock().await.read(body.as_mut_slice()).await?;
+                stream.lock().await.read_exact(body.as_mut_slice()).await?;
                 debug!("{:?}", body.clone());
                 let mut checksum: [u8; 16] = [0; 16];
-                stream.lock().await.read(&mut checksum).await?;
+                stream.lock().await.read_exact(&mut checksum).await?;
                 let packet = Packet {
                     heady: PacketHeady { header, body },
                     checksum,
